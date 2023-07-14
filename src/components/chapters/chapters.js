2632e352b38f8bcd909589646dddf3cd72d495c1
Eddie Posey <eposey@criticalmass.com>
2023-07-14 10:51:03 -0400

    updates

--- src/components/chapters/chapters.js
+++ src/components/chapters/chapters.js
@@ -8,7 +8,7 @@ gsap.registerPlugin(ScrollTrigger)
 
 const chaptersTimeline = gsap.timeline({
   paused: true,
-  ease: 'power1.inOut',
+  ease: 'Power0.easeNone',
 })
 
 const setScrollRailPosition = () => {
@@ -38,7 +38,7 @@ const cursorAnimation = (textElementID) => {
   const textElement = document.querySelector(textElementID)
 
   if(!textElement) {
-    console.error("can't find a text element with that id. either it is authored incorrectly or the component needs to be reworked based on authoring capabilities. please reach out to the dev team.")
+    console.error("this element could potentially be missing a css id. either it is authored incorrectly or the component needs to be reworked based on authoring capabilities. please reach out to the dev team.")
     return
   }
 
@@ -69,19 +69,20 @@ const captionFade = () => {
 const imageScale = () => {
   const imageContainer = document.getElementById('chapters-image')
   const animationInnerContainer = document.getElementById('chapters-animation-inner')
-  const initialImagePosition = { right: 0, top: 0, bottom: 0, left: 0, width: '100%' }
+  const initialImagePositionMobile = { height: '100vh', right: 0, top: 0, bottom: 0, left: 0, width: '100%' }
+  const initialImagePositionDesktop = { right: 0, top: 0, bottom: 0, left: 0, width: '100%' }
 
 	ScrollTrigger.matchMedia({
 		'(min-width: 1035px)': function () {
       const finalImagePosition = { right: 40, top: 40, bottom: 40, left: '50%', width: 'initial', duration: 1.25 }
-      chaptersTimeline.fromTo(imageContainer, initialImagePosition, finalImagePosition)
+      chaptersTimeline.fromTo(imageContainer, initialImagePositionDesktop, finalImagePosition)
       console.log(1)
     },
 		'(max-width: 1035px)': function () {
       const animationInnerPadding = { padding: '40px 40px 0 40px'}
       const finalImagePosition = { height: '340px', duration: 1.25, }
       chaptersTimeline.to(animationInnerContainer, animationInnerPadding)
-      chaptersTimeline.fromTo(imageContainer, initialImagePosition, finalImagePosition, '<')
+      chaptersTimeline.fromTo(imageContainer, initialImagePositionMobile, finalImagePosition, '<')
     },
 	})
 }
@@ -91,8 +92,8 @@ const scrollText = () => {
 
   ScrollTrigger.matchMedia({
 		'(min-width: 1035px)': function () {
-      chaptersTimeline.to(chaptersAnimatioinText, { top: 0, opacity: 1, duration: 2, delay: 0.2 })
-      chaptersTimeline.to(chaptersAnimatioinText, { top: 'initial', bottom: 0, duration: 3, delay: 0.2 })
+      chaptersTimeline.to(chaptersAnimatioinText, { top: 0, opacity: 1, duration: 4, delay: 0.2 })
+      chaptersTimeline.to(chaptersAnimatioinText, { top: 'initial', bottom: 0, duration: 5, delay: 0.2 })
     },
 		'(max-width: 1035px)': function () {
       chaptersTimeline.to(chaptersAnimatioinText, { top: '0', opacity: 1, duration: 2 })
@@ -106,7 +107,7 @@ const animateInnerContainer = () => {
   chaptersTimeline.to(animationInnerContainer, { top: 'initial', bottom: 0, duration: 8 })
 }
 
-const animationTimeline = (textElementsByID) => {
+const chaptersAnimation = (textElementsByID) => {
   setScrollRailPosition()
   textElementsByID.forEach(element => cursorAnimation(element))
   captionFade()
@@ -123,4 +124,4 @@ const animationTimeline = (textElementsByID) => {
 
 const textElementsByID = ['#chapters-text-1', '#chapters-text-2', '#chapters-text-3']
 
-scrollPin('chapters-animation', animationTimeline(textElementsByID))
+scrollPin('chapters-animation', chaptersAnimation(textElementsByID))
