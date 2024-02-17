00c581a4df541bc4df1df616f6a18f8c9e5b7d94
Eddie Posey <edwardposey@icloud.com>
2024-02-17 12:34:20 -0500

    cleanup and updates

--- src/components/chap/chap.js
+++ src/components/chap/chap.js
@@ -19,10 +19,11 @@ const simpleInit = () => {
   const simpleScrollHandlersInitiated = window['simpleChapters']
   const simpleChapters = document.querySelectorAll('[data-type="simple"]')
 
+  // checks to prevent multiple scroll handlers being added
   if (simpleChapters.length === 0) return
   if (simpleScrollHandlersInitiated) return
 
-  // typewriter effect goes here
+  // desktop and mobile animations
   simpleChapters.forEach((element) => scrollPin(element.querySelector(timeline), scaleBG(element.querySelector('.ch-anim'))))
 
   window['simpleChapters'] = true
@@ -32,16 +33,18 @@ const enhancedInit = () => {
   const enhancedScrollHandlersInitiated = window['enhancedChapters']
   const enhancedChapters = document.querySelectorAll('[data-type="enhanced"]')
 
+  // checks to prevent multiple scroll handlers being added
   if (enhancedChapters.length === 0) return
   if (enhancedScrollHandlersInitiated) return
 
   enhancedChapters.forEach((element) => {
-    // typewriter effect goes here
+
+    // desktop animations
     scrollPin(element.querySelector(deskTime), scaleBG(element.querySelector(deskAnim)), true)
     scrollDistance(element.querySelector(deskRail), scrollFade(element.querySelector(deskRail)), 800, 0)
-    // typewriter effect goes here
-    scrollPin(element.querySelector(mobiTime), () => {})
 
+    // mobile animations
+    scrollPin(element.querySelector(mobiTime), () => {})
     ScrollTrigger.create({
       trigger: element.querySelector(mobiRail),
       start: 'top 500px',
@@ -64,12 +67,17 @@ const plusInit = () => {
   const plusScrollHandlersInitiated = window['plusChapers']
   const plusChapters = document.querySelectorAll('[data-type="plus"]')
 
+  // checks to prevent multiple scroll handlers being added
   if (plusChapters.length === 0) return
   if (plusScrollHandlersInitiated) return
 
   plusChapters.forEach((element) => {
+
+    // desktop animations
     scrollPin(element.querySelector(deskTime), () => {}, true)
     scrollDistance(element.querySelector(deskRail), scrollFade(element.querySelector(deskRail)), 800, 0)
+
+    // mobile animations
     scrollPin(element.querySelector(mobiTime), () => {})
     scrollDistance(element.querySelector(mobiRail), () => {}, 600, 400)
     scrollDistance(element.querySelector(mobiRail), () => {}, 500, 400)
